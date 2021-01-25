8ff2c6a7ec610a5976ef68d15f1a3a924d874bd7
Leonel Parrales <dparrales@est.ups.edu.ec>
2021-01-24 22:24:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -124,6 +124,16 @@ const data = [
   },
 ];
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
+    res.send();
+  });
+});
+
 app.get("/data", (request, response) => {
   response.json(data);
 });
